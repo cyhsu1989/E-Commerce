d8311705c55ffc148f03c9914056157aec40c486
Chin-Yu Hsu <sw9856@gmail.com>
2015-09-15 21:26:21 +0800

    Adjust
    細部調整

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+			//SliderShow用
 			$('.slideShowContainer').slick({
 				infinite: true,
 				autoplay: true,
@@ -46,30 +46,6 @@ $(document).ready(function(){
 			//Retina Image	
 			$('.retina').retina();
 
-			//SliderShow用
-    		$('.iosSlider').iosSlider({
-		        snapToChildren: true,
-		        scrollbar: true,
-		        scrollbarHide: false,
-		        desktopClickDrag: true,
-		        scrollbarLocation: 'bottom',
-		        scrollbarHeight: '6px',
-		        //scrollbarBackground: 'url(_img/some-img.png) repeat 0 0',
-		        scrollbarBorder: '1px solid #000',
-		        scrollbarMargin: '0 30px 16px 30px',
-		        scrollbarOpacity: '0.75',
-		        //onSlideChange: slideChange,
-		        autoSlide: true,
-		        autoSlideTimer: 5000,
-		        autoSlideTransTimer: 1000,
-		        autoSlideHoverPause: true,
-		        infiniteSlider: true,
-		        stageCSS: {
-		        	
-		        },
-		        navPrevSelector: $('.prev'),
-		        navNextSelector: $('.next')
-		    });
 
     	    //GoToTop
     	    $("#goTop").hide();
