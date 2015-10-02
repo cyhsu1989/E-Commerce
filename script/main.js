a2a7009443ffee181cbeda588fd02c74264fc533
Chin-Yu Hsu <sw9856@gmail.com>
2015-10-03 00:34:08 +0800

    RWD#6
    大部分響應完成!!

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 			//SliderShow用
-			$('.slideShowContainer').slick({
+		 	$('.slideShowContainer').slick({
 				infinite: true,
 				autoplay: true,
    				autoplaySpeed: 3000,
@@ -16,104 +16,178 @@ $(document).ready(function(){
 	  					settings:{
 	  						variableWidth: false,
 	  						centerMode: false,
-	  						infinite: true,
-	  						slidesToShow: 1,
-	  						slidesToScroll: 1
-	  					}
-  					},
-  					{
-	  					breakpoint: 769,
-	  					settings:{
-	  						variableWidth: false,
-	  						centerMode: false,
-	  						infinite: true,
 	  						slidesToShow: 1,
 	  						slidesToScroll: 1
 	  					}
   					}
-  					
   				]
 			})
 
-			//SlideShow for Recent Products
-			$('.recentSlide').slick({
-   				infinite: true,
-  				slidesToShow: 4,
-  				slidesToScroll: 4,
-  				variableWidth: true,
-  				prevArrow: '.recentPrevButton',
-  				nextArrow: '.recentNextButton',
-  				responsive: [
-  					{
-  						breakpoint: 1025,
-						settings:{
-							variableWidth: false,
-							slidesToShow: 3,
-  							slidesToScroll: 3
-						}
-					},	
-					{
-						breakpoint: 769,
-					    settings: "unslick"
-					},
-					{
-						breakpoint: 569,
-					    settings: "unslick"
-					}
-				]
-  			});
-  			//SlideShow for Featured Products
-  			$('.featuredSlide').slick({
-   				infinite: true,
-  				slidesToShow: 4,
-  				slidesToScroll: 4,
-  				variableWidth: true,
-  				prevArrow: '.featuredPrevButton',
-  				nextArrow: '.featuredNextButton',
-  				responsive: [
-  					{
-  						breakpoint: 1025,
-						settings:{
-							variableWidth: false,
-							slidesToShow: 3,
-  							slidesToScroll: 3
-						}
-					},	
-  					{
-						breakpoint: 769,
-					    settings: "unslick"
-					},
-					{
-						breakpoint: 569,
-					    settings: "unslick"
-					}
-					
-				]
-  			});
-			
-			//SlideShow for sponsor
-  			$('.sponsorSlide').slick({
-   				infinite: true,
-   				autoplay: true,
-   				autoplaySpeed: 3000,
-  				slidesToShow: 5,
-  				slidesToScroll: 1,
-  				variableWidth: true,
-  				prevArrow: '.sponsorPrevButton',
-  				nextArrow: '.sponsorNextButton',
-  				responsive: [
-  					{
-						breakpoint: 1025,
-						settings:{
-							variableWidth: false,
-							slidesToShow: 4,
-  							slidesToScroll: 1
-						}
-					    	
-					}
-				]
-  			});
+		 	//SlideShow for Recent Products
+        			$('.recentSlide').slick({
+        				infinite: true,
+		  				slidesToShow: 4,
+		  				slidesToScroll: 4,
+		  				variableWidth: true,
+		  				prevArrow: '.recentPrevButton',
+		  				nextArrow: '.recentNextButton',
+		  				responsive: [
+		  					{
+		  						breakpoint: 1211,
+		  						settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							},
+							{
+		  						breakpoint: 1025,
+		  						settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							}
+						]
+        			});
 
+        			//SlideShow for Featured Products
+		  			$('.featuredSlide').slick({
+		   				infinite: true,
+		  				slidesToShow: 4,
+		  				slidesToScroll: 4,
+		  				variableWidth: true,
+		  				prevArrow: '.featuredPrevButton',
+		  				nextArrow: '.featuredNextButton',
+		  				responsive: [
+		  					{
+		  						breakpoint: 1211,
+		  						settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							},
+		  					{
+		  						breakpoint: 1025,
+								settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							}
+						]
+		  			});
+
+		  			//SlideShow for sponsor
+		  			$('.sponsorSlide').slick({
+		   				infinite: true,
+		   				autoplay: true,
+		   				autoplaySpeed: 3000,
+		  				slidesToShow: 5,
+		  				slidesToScroll: 1,
+		  				variableWidth: true,
+		  				prevArrow: '.sponsorPrevButton',
+		  				nextArrow: '.sponsorNextButton',
+		  				responsive: [
+		  					{
+								breakpoint: 1211,
+								settings:{
+									variableWidth: false,
+									slidesToShow: 4,
+		  							slidesToScroll: 1
+								}
+							}
+						]
+		  			});
+
+			//enquire.js
+  			enquire.register("screen and (max-width: 768px)",{
+  				match : function() {
+  					$('.recentSlide').slick('unslick');
+  					$('.featuredSlide').slick('unslick');
+  					$('.sponsorSlide').slick('unslick');
+  				},
+  				unmatch : function() {
+  					//SlideShow for Recent Products
+        			$('.recentSlide').slick({
+        				infinite: true,
+		  				slidesToShow: 4,
+		  				slidesToScroll: 4,
+		  				variableWidth: true,
+		  				prevArrow: '.recentPrevButton',
+		  				nextArrow: '.recentNextButton',
+		  				responsive: [
+		  					{
+		  						breakpoint: 1211,
+		  						settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							},
+							{
+		  						breakpoint: 1025,
+		  						settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							}
+						]
+        			});
+
+        			//SlideShow for Featured Products
+		  			$('.featuredSlide').slick({
+		   				infinite: true,
+		  				slidesToShow: 4,
+		  				slidesToScroll: 4,
+		  				variableWidth: true,
+		  				prevArrow: '.featuredPrevButton',
+		  				nextArrow: '.featuredNextButton',
+		  				responsive: [
+		  					{
+		  						breakpoint: 1211,
+		  						settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							},
+		  					{
+		  						breakpoint: 1025,
+								settings:{
+									variableWidth: false,
+									slidesToShow: 3,
+		  							slidesToScroll: 3
+								}
+							}
+						]
+		  			});
+
+		  			//SlideShow for sponsor
+		  			$('.sponsorSlide').slick({
+		   				infinite: true,
+		   				autoplay: true,
+		   				autoplaySpeed: 3000,
+		  				slidesToShow: 5,
+		  				slidesToScroll: 1,
+		  				variableWidth: true,
+		  				prevArrow: '.sponsorPrevButton',
+		  				nextArrow: '.sponsorNextButton',
+		  				responsive: [
+		  					{
+								breakpoint: 1211,
+								settings:{
+									variableWidth: false,
+									slidesToShow: 4,
+		  							slidesToScroll: 1
+								}
+							}
+						]
+		  			});
+    			}
+  			});
 
 			//Retina Image	
 			$('.retina').retina();
